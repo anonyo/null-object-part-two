19a96ad601cf337f7f1c76b06b248efbb6e20081
Anonyo Hossain <anonyo.hossain@gmail.com>
2015-08-15 16:12:43 -0400

    Use the free_trial and no_subscription classes To refactor the User class

--- lib/user.rb
+++ lib/user.rb
@@ -2,40 +2,29 @@ class User
   include ActiveModel::Model
   attr_accessor :created_at, :credit_card, :subscription
 
-  FREE_TRIAL = 'Free Trial'
-  NO_PLAN = 'No Plan'
-
   def charge
-    unless subscription.nil?
-      subscription.charge(credit_card)
-    end
+    subscription.charge(credit_card)
   end
 
   def has_mentoring?
-    free_trial? || subscription && subscription.has_mentoring?
+    subscription.has_mentoring?
   end
 
   def price
-    if free_trial? || subscription.nil?
-      0
-    else
-      subscription.price
-    end
+    subscription.price
   end
 
   def plan_name
-    if free_trial?
-      FREE_TRIAL
-    elsif subscription
-      subscription.plan_name
-    else
-      NO_PLAN
-    end
+    subscription.plan_name
   end
 
   private
 
   def free_trial?
     created_at >= 30.days.ago
+  end
+
+  def subscription
+    free_trial? ?  FreeTrial.new : @subscription || NoSubscription.new
   end
 end
